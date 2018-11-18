81b5c289ac14b3a818933d9b4bb72f523616d235
panidomu <panidomunn@gmail.com>
2018-11-18 14:56:07 +0300

    Homework 03 v2 : the code works, there are no errors, but the algorithm is wrong

--- Homework_3/src/geekbrains/java1/Main.java
+++ Homework_3/src/geekbrains/java1/Main.java
@@ -8,56 +8,44 @@ package geekbrains.java1;
 // «Повторить игру еще раз?
 // 1 – да / 0 – нет»(1 – повторить, 0 – нет).
 
-//Вариант 2 ************************************************
-
+//import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
-    Scanner scanner = new Scanner(System.in);
-    Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int x = guessNumber("Введите целое число от  0 до 9  (включительно):  ");  //вызываем  метод?
-        int y = playNew(); //вызываем  метод?
-        System.out.println("Повторить игру еще раз? 1 - да, 0 -- нет ");
-    }
 
-    public static int guessNumber(int player, int guess);  //определяем метод?
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число от  0 до 9  (включительно):  ");
+        int player = scanner.nextInt(); // возвращаем введенное число из сканера в переменную player
+        System.out.println("Вы ввели число " + player);
+        Random randomNumber = new Random(); // генерируем случайное число randomNumber
+        int guess = randomNumber.nextInt(10); // возвращаем сгенерированное число от 0 до 9 в переменную guess
+        System.out.println(guess); //!!!!!!!! временная строка для контроля !!!!!!!!!!!!!!!!
+        //while (int nextPlay ==1) {
 
-    {
-        do {
-            int i = 0;
-            int player = scanner.nextInt(); // возвращаем введенное число из сканера в переменную player
-            System.out.println("Вы ввели число " + player);
-            Random randomNumber = new Random(); // генерируем случайное число randomNumber
-            int guess = randomNumber.nextInt(10); // возвращаем сгенерированное число от 0 до 9 в переменную guess
-            System.out.println(guess); //!!!!!!!! временная строка для контроля , потом уберу !!!!!!!!!!!!!!!!
-
-            if (player < guess) {
-                System.out.println("Ваше число меньше загаданного. Попытайтесь угадать еще раз: ");
-                i++;
+        if (player < guess) {
+            System.out.println("Ваше число меньше загаданного. Попытайтесь угадать еще раз: ");
+        } else {
+            if (player > guess) {
+                System.out.println("Ваше число больше загаданного. Попытайтесь угадать еще раз: ");
             } else {
-                if (player > guess) {
-                    System.out.println("Ваше число больше загаданного. Попытайтесь угадать еще раз: ");
-                    i++;
-                } else {
-                    System.out.println("Вы угадали!");
-                }
+                System.out.println("Вы угадали!");
             }
-        } while (i = 3);
-    }
+            for (int i = 0; i <= 3; i++) {
+            }
+        }
+        System.out.println("Сыграем еще? 1 - да, 0 -- нет ");
 
-    public static int playNew(int nextPlay);  //определяем метод?
-
-    int nextPlay = scan.nextInt();
-     if(nextPlay ==1);
-    {
-        int x = guessNumber("Введите целое число от  0 до 9  (включительно):  ");  //как здесь еще раз вызвать метод?
+        Scanner scan = new Scanner(System.in);
+        int nextPlay = scan.nextInt();
+        if (nextPlay == 1) ;
+        {
+            // продолжить игру. Как ?
+        }
+        // }
+        System.out.println("Спасибо за игру!");
     }
-         System.out.println("Спасибо за игру!");
 }
-
 
 
